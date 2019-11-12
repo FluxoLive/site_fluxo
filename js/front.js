476499febdea2db90440576b986bccddf5f1ec11
Edson Mallet <edsonmmr@gmail.com>
2019-11-12 16:42:46 -0300

    Correções

--- js/front.js
+++ js/front.js
@@ -82,4 +82,21 @@ $(function() {
   $(".btn").on("mouseover mouseout", function() {
     $(this).toggleClass("animated pulse");
   });
+
+  /**
+   * Email Trello
+   * edsonmallet+qqhmypnuzuvgnppzyini@boards.trello.com
+   */
+  $("#formParceiro").submit(function(e) {
+    e.preventDefault();
+    Email.send({
+      Host: "smtp.yourisp.com",
+      Username: "username",
+      Password: "password",
+      To: "them@website.com",
+      From: "you@isp.com",
+      Subject: "This is the subject",
+      Body: "And this is the body"
+    }).then(message => alert(message));
+  });
 });
